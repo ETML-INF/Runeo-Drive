126e3ebed5adf33ee2247db6ab6153cbc9a18242
Yvann Butticaz <34337152+Vidivanze@users.noreply.github.com>
2021-12-16 14:16:47 +0100

    Add last hour check, must move the code in a utils function

--- src/runs/list/ListRunsItem.component.tsx
+++ src/runs/list/ListRunsItem.component.tsx
@@ -16,14 +16,20 @@ export class ListRunsItemComponent extends React.PureComponent<ListRunsItemCompo
         super(props);
     }
 
-
+ 
     render() {
         const {onSelectRun, run} = this.props;
+        
+        //A placer dans une fonction
+        var ONE_HOUR = 60 * 60 * 1000;
+        var now = new Date();
+        console.log((now.getTime() - run.updated_at) < ONE_HOUR);
+        
+        
 
         return (
-            <ListItem bottomDivider onPress={() => onSelectRun(run)}>
+            <ListItem bottomDivider onPress={() => onSelectRun(run)} containerStyle={ (now.getTime() - run.updated_at) < ONE_HOUR ? {backgroundColor:"#a3d1f9"} : false  }>
                 {getRunStatusIcon(run.status)}
-
                 <ListItem.Content>
                     <ListItem.Title style={{fontFamily: 'Montserrat-Medium'}}>{`${run.title.toUpperCase()} ${run.waypoints.get(0)?.nickname}`}</ListItem.Title>
                     <ListItem.Subtitle style={{color: Colors.GREY, fontFamily: 'Montserrat-Regular'}}>{
@@ -37,5 +43,5 @@ export class ListRunsItemComponent extends React.PureComponent<ListRunsItemCompo
             </ListItem>
         )
     }
-
+    
 }
