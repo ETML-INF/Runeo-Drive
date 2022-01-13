45190165c7f7a902a00168d954167e40899f430a
zaidschouwey98 <zazar981@hotmail.com>
2022-01-13 13:41:53 +0100

    wip

--- src/Enrollment.component.tsx
+++ src/Enrollment.component.tsx
@@ -6,86 +6,68 @@ import { ButtonComponent } from "./common/component/ButtonComponent";
 import { TextInputComponent } from "./common/component/TextInput.component";
 
 const RunnersEnrollment = (props: any) => {
-    const initialValues = {
-        token: ""
-    };
     const [response, setResponse] = useState(<div></div>);
-    function getUserState() {
-        return Axios.get("/me").then((res) => {
-            return res.data.status
-        })
-    }
-    useEffect(() => {
-        getUserState().then((status) => {
-            if (status) {
-                switch (status) {
-                    case "hired":
-                    case "taken":
-                    case "free":
-                    case "not-present":
-                        props.setStateValidation(1);
-                        break;
-                    case "inactive":
-                        setResponse(<View>On n'a pas besoin de toi, Merci.</View>)
-                        break;
-                    case "requested":
-                        setResponse(
-                           
-                                    <View>
-                                        <Text style={{ fontFamily: 'Montserrat-ExtraBold', marginLeft: 10 }}>Participer en tant que conducteur pour le Runeo de cette année ?</Text>
-                                        <View style={{marginTop:10}}>
-                                        <ButtonComponent
-                                            title="Je participe"
-                                            onPress={()=>setNewState(3)}
-                                        />
-                                        </View>
-                                        <View style={{marginTop:10}}>
-                                        <ButtonComponent
-                                            title="Je ne participe pas"
-                                            onPress={()=>setNewState(1)}
-                                        />
-                                        </View>
-                                        <View style={{marginTop:10}}>
-                                        <ButtonComponent
-                                            title="Je ne veux plus jamais participer en tant que conducteur"
-                                            onPress={()=>setNewState(8)}
-                                        />
-                                        </View>
-                                    </View>
-                            
+    let response = <div></div>;
+  
+            switch (props.userState) {
+                case "inactive":
+                    setResponse(<View>On n'a pas besoin de toi, Merci.</View>)
+                    break;
+                case "requested":
+                    setResponse(
 
-                        
-                        )
-                        break;
-                    case "retired":
-                        setResponse(<View>Vous n'êtes plus sollicité pour runeo.</View>)
-                        break;
-                    case "confirmed":
-                        setResponse(<View>Validation des données du permis...</View>)
-                        break;
-                    case "validated":
-                        setResponse(<View>En attente de la validation d'engagement de la part d'un administrateur.</View>)
-                        break;
-                    default:
-                        break;
-                }
+                        <View>
+                            <Text style={{ fontFamily: 'Montserrat-ExtraBold', marginLeft: 10 }}>Participer en tant que conducteur pour le Runeo de cette année ?</Text>
+                            <View style={{ marginTop: 10 }}>
+                                <ButtonComponent
+                                    title="Je participe"
+                                    onPress={() => setNewState("confirmed",3)}
+                                />
+                            </View>
+                            <View style={{ marginTop: 10 }}>
+                                <ButtonComponent
+                                    title="Je ne participe pas"
+                                    onPress={() => setNewState("inactive",1)}
+                                />
+                            </View>
+                            <View style={{ marginTop: 10 }}>
+                                <ButtonComponent
+                                    title="Je ne veux plus jamais participer en tant que conducteur"
+                                    onPress={() => setNewState("retired",8)}
+                                />
+                            </View>
+                        </View>
+
+
+
+                    )
+                    break;
+                case "retired":
+                    setResponse(<View>Vous n'êtes plus sollicité pour runeo.</View>)
+                    break;
+                case "confirmed":
+                    setResponse(<View>Validation des données du permis...</View>)
+                    break;
+                case "validated":
+                    setResponse(<View>En attente de la validation d'engagement de la part d'un administrateur.</View>)
+                    break;
+                default:
+                    break;
             }
 
-        });
-    }, []);
 
-    async function setNewState(state:Number) {
+    async function setNewState(stateName: string,stateId:number) {
         let user = JSON.parse(localStorage.getItem("authenticatedUser")!);
-        Axios.patch(`/users/${user.id}`,{"status":state}).then((res)=>{
-            props.setStateValidation(1);
-        }).catch((err)=>{console.log(err)})
+        Axios.patch(`/users/${user.id}`, { "status": stateId }).then((res) => {
+            props.setUserState(stateName);
+        }).catch((err) => { console.log(err) })
     }
     return (
         <SafeAreaView style={styles.wrapper}>
             <View style={styles.textCenter}>
                 <Text style={{ fontFamily: 'Montserrat-SemiBold', fontSize: 30, marginBottom: 50 }}>Processus d'inscription</Text>
             </View>
-            <View style={{marginTop:10}}>
+            <View style={{ marginTop: 10 }}>
                 {response}
             </View>
         </SafeAreaView>
--- src/Router.component.tsx
+++ src/Router.component.tsx
@@ -1,15 +1,16 @@
 import React, { useState } from "react";
-import {AuthContainer} from "./Provider.component";
-import {AuthComponent} from "./auth/Auth.component";
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import {ListUsersComponent} from "./users/listUsers.component";
-import {Icon} from "react-native-elements";
-import {RunsComponent} from "./runs/Runs.component";
-import {VehiclesComponent} from "./vehicles/Vehicles.components";
-import {ListFastDialsComponent} from "./fastDials/FastDials.component";
-import {NotificationsComponent} from "./notifications/Notifications.component";
-import {Colors} from "./common/utils/Color.utils";
+import { AuthContainer, UsersContainer } from "./Provider.component";
+import { AuthComponent } from "./auth/Auth.component";
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { ListUsersComponent } from "./users/listUsers.component";
+import { Icon } from "react-native-elements";
+import { RunsComponent } from "./runs/Runs.component";
+import { VehiclesComponent } from "./vehicles/Vehicles.components";
+import { ListFastDialsComponent } from "./fastDials/FastDials.component";
+import { NotificationsComponent } from "./notifications/Notifications.component";
+import { Colors } from "./common/utils/Color.utils";
 import RunnersEnrollment from "./Enrollment.component";
+import Axios from "axios";
 
 const Tab = createBottomTabNavigator();
 
@@ -19,68 +20,83 @@ const ACTIVE_TAB_COLOR = Colors.BLUE;
 const INACTIVE_TAB_COLOR = Colors.BLACK;
 
 export function RouterComponent() {
-    const [stateValidation, setStateValidation] = useState();
+    const [userState, setUserState] = useState("");
     const authContainer = AuthContainer.useContainer();
 
     const tabBarIconGen = (name: string) => {
-        return ({focused}: { focused: boolean }) => (<Icon
+        return ({ focused }: { focused: boolean }) => (<Icon
             type='font-awesome'
             name={name}
             color={focused ? ACTIVE_TAB_COLOR : INACTIVE_TAB_COLOR}
         />)
     };
-    
+
+    const getUserState = () => {
+        return Axios.get("/me").then((res) => {
+            return res.data.status
+        })
+    }
     if (authContainer.authenticatedUser) {
-        if(!stateValidation){
-            return(
-                <RunnersEnrollment setStateValidation={setStateValidation}/>
-            )
+        if (!userState) {
+            getUserState().then((status) => {
+                setUserState(status);
+            })
+        } else 
+        switch (userState) {
+            case "hired":
+            case "taken":
+            case "free":
+            case "not-present":
+                return (
+                    <Tab.Navigator initialRouteName={RUNS_TAB} tabBarOptions={{
+                        activeTintColor: ACTIVE_TAB_COLOR,
+                        inactiveTintColor: INACTIVE_TAB_COLOR,
+                    }}>
+                        <Tab.Screen
+                            name={RUNS_TAB}
+                            options={{
+                                tabBarIcon: tabBarIconGen('list'),
+                            }}
+                            component={RunsComponent}
+                        />
+                        <Tab.Screen
+                            name="Drivers"
+                            options={{
+                                tabBarIcon: tabBarIconGen('drivers-license-o'),
+                            }}
+                            component={ListUsersComponent}
+                        />
+                        <Tab.Screen
+                            name="Vehicles"
+                            options={{
+                                tabBarIcon: tabBarIconGen('car'),
+                            }}
+                            component={VehiclesComponent}
+                        />
+                        <Tab.Screen
+                            name="Notifications"
+                            options={{
+                                tabBarIcon: tabBarIconGen('bell'),
+                            }}
+                            component={NotificationsComponent}
+                        />
+                        <Tab.Screen
+                            name="Utils"
+                            options={{
+                                tabBarIcon: tabBarIconGen('info-circle'),
+                            }}
+                            component={ListFastDialsComponent}
+                        />
+                    </Tab.Navigator>
+                )
+            default:
+                console.log(userState)
+                return (<RunnersEnrollment userState={userState} setUserState={setUserState}/>)
         }
-        return (
-            <Tab.Navigator initialRouteName={RUNS_TAB} tabBarOptions={{
-                activeTintColor: ACTIVE_TAB_COLOR,
-                inactiveTintColor: INACTIVE_TAB_COLOR,
-            }}>
-                <Tab.Screen
-                    name={RUNS_TAB}
-                    options={{
-                        tabBarIcon: tabBarIconGen('list'),
-                    }}
-                    component={RunsComponent}
-                />
-                <Tab.Screen
-                    name="Drivers"
-                    options={{
-                        tabBarIcon: tabBarIconGen('drivers-license-o'),
-                    }}
-                    component={ListUsersComponent}
-                />
-                <Tab.Screen
-                    name="Vehicles"
-                    options={{
-                        tabBarIcon: tabBarIconGen('car'),
-                    }}
-                    component={VehiclesComponent}
-                />
-                <Tab.Screen
-                    name="Notifications"
-                    options={{
-                        tabBarIcon: tabBarIconGen('bell'),
-                    }}
-                    component={NotificationsComponent}
-                />
-                <Tab.Screen
-                    name="Utils"
-                    options={{
-                        tabBarIcon: tabBarIconGen('info-circle'),
-                    }}
-                    component={ListFastDialsComponent}
-                />
-            </Tab.Navigator>
-        )
+        
     }
 
     return (
-        <AuthComponent/>
+        <AuthComponent />
     )
 }
