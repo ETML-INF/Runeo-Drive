74e38b7203b03306ece34df99fa3e5d8e92d6335
AndiSO-1 <andi.santos-oliveira@cpnv.ch>
2022-02-11 13:22:34 +0100

    Rename style

--- src/common/component/text/MeetingText.component.tsx
+++ src/common/component/text/MeetingText.component.tsx
@@ -3,11 +3,11 @@ import React, {PropsWithChildren} from "react";
 import {Colors} from "../../utils/Color.utils";
 
 export function MeetingTextComponent(props: PropsWithChildren<any>) {
-    return (<Text style={styles.importantText}>{props.children}</Text>)
+    return (<Text style={styles.meetingText}>{props.children}</Text>)
 }
 
 const styles = StyleSheet.create({
-    importantText: {
+    meetingText: {
         fontWeight: "bold",
         backgroundColor: Colors.LIGHT_BLUE,
         borderRadius: 25,
