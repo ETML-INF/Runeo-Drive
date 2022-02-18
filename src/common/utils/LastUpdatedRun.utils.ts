8d9b1870ca2f67b5595c88d3eb4b72f9e70437cf
Yvann Butticaz <34337152+Vidivanze@users.noreply.github.com>
2022-02-18 08:16:21 +0100

    Update LastUpdatedRun.utils.ts

--- src/common/utils/LastUpdatedRun.utils.ts
+++ src/common/utils/LastUpdatedRun.utils.ts
@@ -8,9 +8,14 @@ export function lastUpdatedRun(run :RunResource, userId: any): Boolean {
     const ONE_HOUR = 60 * 60 * 1000;
     let now = new Date();
     
+    //the run was updated during the last hour
     if((now.getTime() - run.updated_at) < ONE_HOUR){
-        //the runner object the authenticated user is driving
-        return !!run.runners.find(runner => runner.user?.id === userId);
+        //the run was not acknowledged after the last update
+        if(run.acknowledged_at < run.updated_at){
+            //the runner object the authenticated user is driving
+            return !!run.runners.find(runner => runner.user?.id === userId);
+        }
+        return false;
     }
     
     return false;
