c83bb74ae0d64419d2c9bb2eb597ce1ec1ebcccd
Yvann Butticaz <34337152+Vidivanze@users.noreply.github.com>
2022-03-17 14:21:09 +0100

    Update LastUpdatedRun.utils.ts

--- src/common/utils/LastUpdatedRun.utils.ts
+++ src/common/utils/LastUpdatedRun.utils.ts
@@ -3,15 +3,20 @@
 */
 
 import {RunResource} from "../resources/Run.resource";
+import {DateTime} from "luxon";
 
 export function lastUpdatedRun(run :RunResource, userId: any): Boolean {
     const ONE_HOUR = 60 * 60 * 1000;
-    let now = new Date();
+    let now = DateTime.local();
     
     //the run was updated during the last hour
-    if((now.getTime() - run.updated_at) < ONE_HOUR){
+    if((now - run.updated_at) < ONE_HOUR){
         //the run was not acknowledged after the last update
-        if(run.acknowledged_at < run.updated_at){
+        if(isNaN(run.acknowledged_at)){
+            //the runner object the authenticated user is driving
+            return !!run.runners.find(runner => runner.user?.id === userId);
+        } 
+        else if(run.updated_at > run.acknowledged_at){
             //the runner object the authenticated user is driving
             return !!run.runners.find(runner => runner.user?.id === userId);
         }
@@ -21,4 +26,8 @@ export function lastUpdatedRun(run :RunResource, userId: any): Boolean {
     return false;
 }
 
+
+function typeOf(acknowledged_at: DateTime) {
+    throw new Error("Function not implemented.");
+}
 
