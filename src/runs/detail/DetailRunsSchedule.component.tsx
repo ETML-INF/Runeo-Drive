e6f20774490e3069ca566646075ee2b3b2605890
AndiSO-1 <andi.santos-oliveira@cpnv.ch>
2022-03-25 09:22:58 +0100

    Edit run date display to be inline

--- src/runs/detail/DetailRunsSchedule.component.tsx
+++ src/runs/detail/DetailRunsSchedule.component.tsx
@@ -1,10 +1,7 @@
-import {Text} from "react-native";
 import React from "react";
 import {RunResource} from "../../common/resources/Run.resource";
 import {DATE_ONLY_FORMAT} from "../../common/utils/Date.utils";
 import {CardComponentWithIcon} from "../../common/component/Card.component";
-import {InlineTextComponent} from "../../common/component/text/InlineText.component";
-import {ImportantTextComponent} from "../../common/component/text/ImportantText.component";
 
 export interface DetailRunsScheduleComponent {
     currentRun: RunResource
@@ -12,11 +9,7 @@ export interface DetailRunsScheduleComponent {
 
 export function DetailRunsScheduleComponent({currentRun}: DetailRunsScheduleComponent) {
     return (
-        <CardComponentWithIcon title={"Date"} icon={"calendar"} >
-            <InlineTextComponent>
-                <Text>Prévu </Text>
-                <ImportantTextComponent>{currentRun.begin_at.toFormat(DATE_ONLY_FORMAT)}</ImportantTextComponent>
-            </InlineTextComponent>
+        <CardComponentWithIcon title={"Date " + currentRun.begin_at.toFormat(DATE_ONLY_FORMAT)} icon={"calendar"} >
         </CardComponentWithIcon>
     )
 }
