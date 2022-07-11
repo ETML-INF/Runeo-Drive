734681ee54e431160e112260d4bbfb61bd322958
XCarrel <Xavier.Carrel@cpnv.ch>
2022-07-11 17:21:51 +0200

    Add Id in the detailed run title

--- src/runs/detail/DetailRunsSchedule.component.tsx
+++ src/runs/detail/DetailRunsSchedule.component.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import {Text, View, StyleSheet} from "react-native"
 import {RunResource} from "../../common/resources/Run.resource";
 import {DATE_ONLY_FORMAT} from "../../common/utils/Date.utils";
 import {CardComponentWithIcon} from "../../common/component/Card.component";
@@ -9,7 +10,15 @@ export interface DetailRunsScheduleComponent {
 
 export function DetailRunsScheduleComponent({currentRun}: DetailRunsScheduleComponent) {
     return (
-        <CardComponentWithIcon title={"Date " + currentRun.begin_at.toFormat(DATE_ONLY_FORMAT)} icon={"calendar"} >
-        </CardComponentWithIcon>
+        <View>
+            <Text style={styles.title}>Run #{ currentRun.id } du { currentRun.begin_at.toFormat(DATE_ONLY_FORMAT)}</Text>
+        </View>
     )
 }
+
+const styles = StyleSheet.create({
+    title: {
+        fontSize: 18,
+        textAlign: "center"
+    },
+})
