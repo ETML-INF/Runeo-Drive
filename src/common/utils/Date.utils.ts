ef1f638cbb689706c31d9045669ef5de2c7adf9a
XCarrel <Xavier.Carrel@cpnv.ch>
2022-07-12 10:51:40 +0200

    Display dates in french

--- src/common/utils/Date.utils.ts
+++ src/common/utils/Date.utils.ts
@@ -1,3 +1,20 @@
-export const DATE_FORMAT = "cccc dd à HH'h'mm"
-export const DURATION_FORMAT = "hh'h'mm"
-export const DATE_ONLY_FORMAT = "cccc dd"
+import moment from "moment";
+
+export const DATE_FORMAT = "cccc dd à HH'h'mm";
+export const DURATION_FORMAT = "hh'h'mm";
+export const DATE_ONLY_FORMAT = "cccc dd";
+
+export function dateWithLocalDay(date) {
+  let res = moment(date);
+  const daysOfWeek = [
+    "Dimanche",
+    "Lundi",
+    "Mardi",
+    "Mercredi",
+    "Jeudi",
+    "Vendredi",
+    "Samedi",
+  ];
+  return daysOfWeek[res.format("d")] + " " + res.format("D");
+  // TODO: find the proper way to do this....
+}
--- src/runs/detail/DetailRunsSchedule.component.tsx
+++ src/runs/detail/DetailRunsSchedule.component.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import {Text, View, StyleSheet} from "react-native"
 import {RunResource} from "../../common/resources/Run.resource";
-import {DATE_ONLY_FORMAT} from "../../common/utils/Date.utils";
+import {dateWithLocalDay} from "../../common/utils/Date.utils";
 import {CardComponentWithIcon} from "../../common/component/Card.component";
 
 export interface DetailRunsScheduleComponent {
@@ -11,7 +11,7 @@ export interface DetailRunsScheduleComponent {
 export function DetailRunsScheduleComponent({currentRun}: DetailRunsScheduleComponent) {
     return (
         <View>
-            <Text style={styles.title}>Run #{ currentRun.id } du { currentRun.begin_at.toFormat(DATE_ONLY_FORMAT)}</Text>
+            <Text style={styles.title}>Run #{ currentRun.id } du { dateWithLocalDay(currentRun.begin_at)}</Text>
         </View>
     )
 }
