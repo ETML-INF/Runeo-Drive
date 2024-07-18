f70109135e50f6ec9123be9dc1113b837099933c
XCarrel <Xavier.Carrel@cpnv.ch>
2024-07-18 09:15:05 +0200

    feat(Run): Allow the user to show the run on google maps

--- src/runs/detail/DetailRunsCourse.component.tsx
+++ src/runs/detail/DetailRunsCourse.component.tsx
@@ -1,12 +1,10 @@
 import {RunResource} from "../../common/resources/Run.resource";
-import {StyleSheet, Text, View} from "react-native";
-import {DURATION_FORMAT} from "../../common/utils/Date.utils";
-import React, {Fragment} from "react";
+import {StyleSheet, Button, Text, TouchableOpacity, Linking, View} from "react-native";
 import {CardComponentWithIcon} from "../../common/component/Card.component";
-import {ImportantTextComponent} from "../../common/component/text/ImportantText.component";
 import { WayPointTextComponent } from "../../common/component/text/WayPointText.component";
-import {InlineTextComponent} from "../../common/component/text/InlineText.component";
 import moment from "moment"
+import {ButtonComponent} from "../../common/component/ButtonComponent";
+import { NetworkContainer } from "../../Provider.component";
 
 export interface CourseDetailRunsComponentProps {
     currentRun: RunResource
@@ -14,6 +12,11 @@ export interface CourseDetailRunsComponentProps {
 
 export function DetailRunsCourseComponent({currentRun}: CourseDetailRunsComponentProps) {
     const runDuration = currentRun.finished_at.diff(currentRun.begin_at);
+    const {isInternetReachable} = NetworkContainer.useContainer();
+
+    const showRunOnMap = () => {
+        Linking.openURL(currentRun.google).catch(err => console.error("Couldn't load page", err));
+    };
 
     return (
         <CardComponentWithIcon title={"Parcours"} icon={"map-marked-alt"}>
@@ -26,6 +29,27 @@ export function DetailRunsCourseComponent({currentRun}: CourseDetailRunsComponen
                         isMeeting={waypoint.is_meeting == 1}
                     />))}
             </View>
+            <View style={styles.container}>
+                <ButtonComponent
+                    title="Visualiser"
+                    disabled={ !isInternetReachable }
+                    color="#f194ff"
+                    onPress={showRunOnMap}
+                />
+
+            </View>
         </CardComponentWithIcon>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    linkText: {
+        color: 'blue',
+        textDecorationLine: 'underline',
+    },
+  });
