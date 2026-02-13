a7d0be2a6171738558ca7a945eb750b9d01cb3ac
XCarrel <Xavier.Carrel@eduvaud.ch>
2026-02-13 11:04:05 +0100

    chore(app): Prepare for merge

--- src/auth/Auth.component.tsx
+++ src/auth/Auth.component.tsx
@@ -9,7 +9,7 @@ export function AuthComponent() {
         <Text style={{ fontFamily: "Montserrat-SemiBold", fontSize: 30, marginBottom: 50 }}>Runeo Drive</Text>
       </View>
       <TokenAuthComponent />
-      <Text style={styles.version}>2025.1.1</Text>
+      <Text style={styles.version}>2026.1.0</Text>
     </SafeAreaView>
   );
 }
